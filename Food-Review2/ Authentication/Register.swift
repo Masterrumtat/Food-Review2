671fc43053fe4c5f6147ea88502718db130e8125
Masterrumtat <captain019377@gmail.com>
2023-11-15 00:04:00 +0700

    push1

---
+++ Food-Review2/ Authentication/Register.swift
@@ -0,0 +1,60 @@
+//
+//  Register.swift
+//  Food-Review2
+//
+//  Created by Vituruch Sinthusate on 14/11/2566 BE.
+//
+
+import UIKit
+import FirebaseAuth
+import FirebaseFirestore
+class Register: UIViewController {
+    
+        @IBOutlet weak var name: UITextField!
+        @IBOutlet weak var email: UITextField!
+        @IBOutlet weak var password: UITextField!
+        @IBOutlet weak var registerBtn: UIButton!
+        
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            // Set up the register button
+            registerBtn.addTarget(self, action: #selector(registerTapped), for: .touchUpInside)
+        }
+        
+        @objc func registerTapped() {
+            // Get the user's name, email address, and password
+            let name = name.text!
+            let email = email.text!
+            let password = password.text!
+            
+            // Create a new user account
+            Auth.auth().createUser(withEmail: email, password: password) { result, error in
+                if let error = error {
+                    // Handle the error
+                    print(error.localizedDescription)
+                } else {
+                    // The user account was created successfully
+                    // Save the user's name to the database
+                    let db = Firestore.firestore()
+                    let userRef = db.collection("users").document(result!.user.uid)
+                    
+                    userRef.getDocument { snapshot, error in
+                        if let error = error {
+                            // Handle the error
+                            print(error.localizedDescription)
+                        } 
+                        else {
+                            // Get the user's name from the snapshot
+                            let name = snapshot?["name"] as? String
+                            
+                            // Save the user's name to the database
+                            userRef.updateData(["name": name as Any])
+                        }
+                    }
+                }
+            }
+        }
+    }
+
--- Food-Review2/ViewController.swift
+++ Food-Review2/ViewController.swift
@@ -6,11 +6,14 @@
 //
 
 import UIKit
-
+import FirebaseCore
 class ViewController: UIViewController {
-
+    
+   
     override func viewDidLoad() {
         super.viewDidLoad()
+            
+
         // Do any additional setup after loading the view.
     }
 
