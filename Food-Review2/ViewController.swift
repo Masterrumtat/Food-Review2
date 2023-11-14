62a7d4572b23ae411fb86c65a6489e3e9d9fff6a
Masterrumtat <captain019377@gmail.com>
2023-11-14 19:51:38 +0700

    Initial Commit

---
+++ Food-Review2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Food-Review2
+//
+//  Created by Vituruch Sinthusate on 14/11/2566 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
