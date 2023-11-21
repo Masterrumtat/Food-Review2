2548c583eee67208ba9e977c46d1db8695122415
Masterrumtat <captain019377@gmail.com>
2023-11-22 00:13:07 +0700

    update 1.database get data 2.test import swiftui

--- Food-Review2/ Authentication/page/Review.swift
+++ Food-Review2/ Authentication/page/Review.swift
@@ -9,113 +9,110 @@ import UIKit
 import FirebaseFirestore
 import Firebase
 
-
-class RestaurantTableViewController: UITableViewController {
+class RestaurantTbVC: UITableViewController {
 
     var restaurants: [DocumentSnapshot] = []
 
-      override func viewDidLoad() {
-          super.viewDidLoad()
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
-          // Call a function to fetch data from Firestore
-          fetchDataFromFirestore()
-      }
-
-      // Function to fetch data from Firestore
-      func fetchDataFromFirestore() {
-          let db = Firestore.firestore()
-
-          db.collection("restaurants").getDocuments { (querySnapshot, error) in
-              if let error = error {
-                  print("Error getting documents: \(error)")
-              } else {
-                  self.restaurants = querySnapshot?.documents ?? []
-                  self.tableView.reloadData()
-              }
-          }
-      }
-
-      // Implement UITableViewDataSource methods
-      override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-          return restaurants.count
-      }
-
-      override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-          let cell = tableView.dequeueReusableCell(withIdentifier: "restCell", for: indexPath) as! rest
-
-          // Customize the cell using data from the Firestore document
-          let restaurant = restaurants[indexPath.row]
-          cell.nameRest.text = restaurant["rest1"] as? String
-          print(restaurant)
-          // Set other UI elements as needed
-
-          return cell
-      }
-  }
-
-func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        // Call a function to fetch data from Firestore
+        fetchDataFromFirestore()
     }
 
-func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+    // Function to fetch data from Firestore
+    func fetchDataFromFirestore() {
+        let db = Firestore.firestore()
+
+        db.collection("restaurants2").getDocuments { (querySnapshot, error) in
+            if let error = error {
+                print("Error getting documents: \(error)")
+            } else {
+                self.restaurants = querySnapshot!.documents
+                self.tableView.reloadData()
+                for document in querySnapshot!.documents {
+                    print("\(document.documentID) => \(document.data())")
+                }
+            }
+        }
     }
 
-    /*
+    // Implement UITableViewDataSource methods
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restaurants.count
+    }
+
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "restCell", for: indexPath) as! rest
 
-        // Configure the cell...
+        let restaurant = restaurants[indexPath.row]
+        cell.nameRest.text = restaurant["name"] as? String
+        
+        
+
+        // Set other UI elements as needed
 
         return cell
     }
-    */
-
+}
     /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
+     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+     let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+     
+     // Configure the cell...
+     
+     return cell
+     }
+     */
+    
     /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
     /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
     /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
     /*
-    // MARK: - Navigation
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 
---
+++ Food-Review2/testUI.swift
@@ -0,0 +1,9 @@
+//
+//  testUI.swift
+//  Food-Review2
+//
+//  Created by Vituruch Sinthusate on 22/11/2566 BE.
+//
+
+import Foundation
+
